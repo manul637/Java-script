dad4387f3c4742da4e7e0396c767eaf0a757e8c2
manul <manulgupta1009@gmail.com>
2025-02-12 22:57:08 +0530

    Java-Script Assignment 2

---
+++ assignment2.js
@@ -0,0 +1,96 @@
+// let num = 40;
+
+// if ( num % 10 == 0) {
+//     console.log("GOOD");
+// } else {
+//     console.log("BAD");
+// }
+
+
+//------------------------------------------------------------------------------------------
+
+
+
+// let name = prompt("Enter  your name :");
+// let age  = prompt("Enter your age : ");
+
+// alert(name + " is " + age + " years old ."); 
+
+
+
+//---------------------------------------------------------------------------------------------
+
+
+
+// let quat = prompt("enter month (1-12) : ");
+
+// switch (quat) {
+//     case (1-3):                                                     wrong way 
+//         console.log("1st Quater");
+//         break;
+//     case (4-6):
+//         console.log("2nd quater");
+//         break;
+//     case (7-9):
+//         console.log("3rd Quarter");
+//         break;
+//     case (10-12):
+//         console.log("4th Quarter");
+//         break;        
+
+//     default: 
+//         console.log("error")
+//         break;
+// }
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
+
+// let str = "manul";
+
+// if ( ( str[0] == 'a' || str [0] == 'A' ) && (str.length > 5 ) ) {
+//     console.log("Golden String ");
+// } else {
+//     console.log("not golden string ");
+// }
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+// let a = 5;
+// let b = 24;
+// let c = 12;
+
+
+// if (a>b) {
+//     if(a>c){
+//         console.log("a is greater");
+//     }else {
+//         console.log("c is greater");
+//     }
+// } 
+// else
+
+// if (b>c) {
+//     console.log("b is greater");
+
+// } else {
+//     console.log("c is greater");
+// }
+
+
+//------------------------------------------------------------------------------------------------------------------
+
+
+
+// let num1 = 32 ;
+// let num2 = 4672;
+
+// if( (num1 % 10) == (num2 %10))  {
+//     console.log(" number ahve same last digit which is " , num1%10);
+// } else {
+//     console.log("number dont have the same last digit")
+// }
