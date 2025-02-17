de2e314a15e6b0e9e6cdc9c07b252fd04178ef1a
manul <manulgupta1009@gmail.com>
2025-02-17 23:29:55 +0530

    Assignment 5

---
+++ assignment5.js
@@ -0,0 +1,29 @@
+// const dice = Math.floor( Math.random()*6) + 1;
+// console.log(dice);
+
+//----------------------------------------------------------------------------------------------------
+
+// const car = {
+//     name : " range rover ",
+//     model : " 1",
+//     color : " white",
+
+// };
+
+// console.log(car.name);
+
+
+//-------------------------------------------------------------------------------------------------------
+
+
+const person = {
+    name : " manul",
+    age : 19,
+    add : " durg",
+
+};
+
+person.add = " singapur"
+person.country = " republic of singapur"
+
+console.log(person);
