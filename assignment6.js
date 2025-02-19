db3b2798409ec855caa3989ced825eae268712c6
manul <manulgupta1009@gmail.com>
2025-02-19 23:48:43 +0530

    Assignment 6

---
+++ assignment6.js
@@ -0,0 +1,33 @@
+// let arr = [ 2 , 7 , 9 , 5 , 2 , 0 , 5 ];
+// let num = 7 ;
+
+// function getNum(arr , num){
+//     for( let i =0 ; i < arr.length ; i++){
+//         if(arr[i]>num){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
+// getNum(arr,num);
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+// let start = promt("enter start range");
+// let end = promt("enter end rrange");
+
+// function getNum(start , end){
+//     let diff = end - start;
+//     return Math.floor(Math.random * diff) + start;    
+// }
+
+// getNum(start,end);
+
+
+//-------------------------------------------------------------------------------------------------------------
