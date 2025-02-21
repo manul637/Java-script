9a79ccc2611c559b55d965c1ac1c64db73beba33
manul <manulgupta1009@gmail.com>
2025-02-22 00:33:54 +0530

    Assignment 6

--- assignment6.js
+++ assignment6.js
@@ -15,6 +15,29 @@
 //-----------------------------------------------------------------------------------------------------------
 
 
+// let str = "abcdabcdefgggh";
+
+
+// function getUnique (str ){
+//     let ans = "";
+
+//     for( let i =0 ; i <str.lenght ; i++){
+//         let currChar = str[i];
+//         if (ans.indexOf(currChar)== -1){
+//             ans =+ currChar;
+//         }
+         
+
+       
+     
+//     }
+//     return ans;
+// }
+
+// getUnique(str);
+
+
+
 
 //-----------------------------------------------------------------------------------------------------------
 
@@ -31,3 +54,49 @@
 
 
 //-------------------------------------------------------------------------------------------------------------
+
+// let str = "apna collage";
+
+
+// function countVowels(str) {
+//     let count = 0;
+//     for ( let i = 0 ; i<str.length ; i++){
+//         if (
+//             str.charAt(i) == 'a' ||
+//             str.charAt(i) == 'e' ||
+//             str.charAt(i) == 'i' ||
+//             str.charAt(i) == 'o' ||
+//             str.charAt(i) == 'u' 
+//         ) {
+//             count ++ ;
+//         }
+
+        
+//     }
+
+//     return count;
+
+// }
+
+
+//----------------------------------------------------------------------------------------------------------
+
+
+let country = [ "australia ", "germany", "india","russia"];
+
+
+function longestNameCountry (country) {
+
+    let ansidx = 0;
+    for (let i = 0 ; i < country.length , i++) {
+
+        let ansLen = country[0].length ;
+        let curLen = country[i].length;
+        if( curLen >ansLen){
+            ansidx = i ;
+        }
+    }
+    return country[ansidx];
+}
+
+longestNameCountry(country);
