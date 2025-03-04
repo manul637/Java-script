adc72762a099cf72a782cda050f7cdc8db1e2d8f
manul <manulgupta1009@gmail.com>
2025-03-04 23:27:10 +0530

    tutorial js

---
+++ index.js
@@ -0,0 +1,54 @@
+// console.log("Manul");
+
+// let pencilPrise = 10;
+// let eraserPrise = 5;
+
+// let output = " the total prise is " + (pencilPrise + eraserPrise) + " rupees.";
+// console.log(output);
+
+
+
+//------------------------------------------------------------------------------------------------------------
+
+
+// STRING METHOD 
+
+//-----------------------------------------------FUNCTIONS --------------------------------------------------
+
+
+// function calc( a , b , c){
+//     let avg = (a +b+ c)/3;
+//     console.log(avg)
+// }
+
+
+// calc(4,7,12);
+
+// -------------------------------------------------------------------------------------------------------------------
+
+// function table(n){
+//     let sum;
+//     for(let i = 1 ; i <= 10; i++){
+//         sum=n*i;
+//         console.log(sum);
+//     }
+// }
+
+// alternate
+
+// function table(n){
+//     for(let i =n ; i<=n*10;i +=n){
+//         console.log(i);
+//     }
+// }
+
+
+// let num = prompt("enter number");
+// table(num);
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
